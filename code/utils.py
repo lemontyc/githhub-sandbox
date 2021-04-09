1b1ba0489a312df19d23908f7e13185453fd8be1
Luis Montoya <luis.montoya@exatec.tec.mx>
2021-04-09 09:20:09 -0500

    [FEAT] Starter python file

---
+++ code/utils.py
@@ -0,0 +1,50 @@
+# What is 5 to the power of 7?
+def pow_5_7():
+    # Include your code in the next line
+    return 
+
+
+# Split this string:
+# s = "Hi there GitHub!"
+# into a list.
+def split_string():
+    s = "Hi there GitHub!"
+    # Include your code in the next line
+    return 
+
+
+# Given the variables:
+# planet = "Earth"
+# diameter = 12742
+# Use .format() to print the following string:
+# The diameter of Earth is 12742 kilometers.
+def format_Earth_diameter():
+    planet = "Earth"
+    diameter = 12742
+    # Include your code in the next lines
+    return
+
+
+# Given this nested list, use indexing to grab the word "hello"
+def get_value_nested_list():
+    lst = [1,4.7,[9,4],[5,[100,200,['hello']],23,11],1,7]
+    # Include your code in the next line
+    return
+
+
+# Given this nested dictionary, get the word "Network".
+def get_value_nested_dict():
+    dict = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'Network']}]}]}
+    # Include your code in the next line
+    return 
+
+
+# Create a function named get_domain, that receives a parameter with an email address, and returns the domain from a string in the form:
+# user@domain.com
+# So for example, passing "user@domain.com" the function returns: domain.com
+# Include your code in the next line
+
+
+# Create a function named multiply, that receives two parameters with a number, and returns the multiplication of both numbers:
+# So for example, passing 5 and 5 to the function returns: 25
+# Include your code in the next line
